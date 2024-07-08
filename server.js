160b17d99e017119583c0db5c384f3a411a49867
Khan Zubair Ahmed <114219018+Kzubair001@users.noreply.github.com>
2024-07-08 20:18:48 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const path = require("path");
+const axios = require("axios");
+const app = express();
+require("dotenv").config();
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "./public")));
+
+app.get("/api/exchange", async (req, res) => {
+  try {
+    const cur1 = req.query.cur1;
+    const apiKey = process.env.API_KEY;
+
+    const response = await axios.get(
+      `https://api.exchangeratesapi.io/latest?base=${cur1}&access_key=${apiKey}`
+    );
+    const currency = response.data;
+
+    res.json(currency);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "An error occurred" });
+  }
+});
+
+app.listen(3001, () => console.log("Listening on 3001 Port"));
